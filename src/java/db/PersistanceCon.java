6d1c5531147d96f7007db052f48e467a9db52cac
dimitrisMaf28 <dbourgiotis@gmail.com>
2014-12-27 19:42:49 +0200

    PersistenceCon added

---
+++ src/java/db/PersistanceCon.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+/**
+ *
+ * @author dimbourgiotis
+ */
+public class PersistanceCon {
+    
+    public void loadAppointments(){
+        
+    }
+    
+    public void saveAppointments(){
+        
+    }
+    
+    public void changeAppointment(){
+        
+    }
+    
+    public void authUsername(){
+        
+    }
+    
+    public void authPassword(){
+        
+    }
+}
