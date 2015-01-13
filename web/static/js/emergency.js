b6b856e3e44a71986112411056ec991ff3cfd15f
gtsop <>
2015-01-13 14:41:04 +0200

    Remove time from datetimepicker

--- web/static/js/emergency.js
+++ web/static/js/emergency.js
@@ -8,7 +8,10 @@ $(function () {
         hidePopup();
     });
     
-    $(".datepicker").datepicker();
+    $(".datepicker").datetimepicker({
+        //nextText: ">",
+        //prevText: "<"
+    });;
 });
 
 
